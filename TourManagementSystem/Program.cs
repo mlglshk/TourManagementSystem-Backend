0e9629d4a8a4cac682c8576c5f72543b8a6a2882
3vavavav3 <3vavavav3@gmail.com>
2026-05-28 22:04:39 +0500

    added port for mila

--- TourManagementSystem/Program.cs
+++ TourManagementSystem/Program.cs
@@ -22,7 +22,8 @@ builder.Services.AddCors(options =>
             "http://localhost:5173", 
             "https://localhost:5173", 
             "http://localhost:3000",
-            "http://localhost:8080")
+            "http://localhost:8080",
+            "http://10.128.97.223:5173")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
